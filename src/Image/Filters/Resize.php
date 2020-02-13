1d2054ecfa8f14438f6a5ed60bc1b607eb44d354
Jan Jablunka <jablunka@gmail.com>
2020-02-13 10:40:22 +0100

    incremental

--- src/Image/Filters/Resize.php
+++ src/Image/Filters/Resize.php
@@ -7,6 +7,9 @@ class Resize extends \Katu\Image\Filter {
 	public function apply($image) {
 		$image->resize($this->params['width'], $this->params['height'], function($constraint) {
 			$constraint->aspectRatio();
+			if ($this->params['noUpsize'] ?? null) {
+				$constraint->upsize();
+			}
 		});
 
 		return true;

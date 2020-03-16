3cd09a322fb9bc22a0c830c0e223685d3fff6d8c
Jan Jablunka <jablunka@gmail.com>
2020-03-16 16:54:03 +0100

    incremental

--- src/Models/UserPasswordToken.php
+++ src/Models/UserPasswordToken.php
@@ -2,26 +2,28 @@
 
 namespace Katu\Models;
 
-class UserPasswordToken extends \Katu\Model {
-
+class UserPasswordToken extends \Katu\Model
+{
 	const TABLE = 'user_password_tokens';
 
 	const EXPIRES = '1 hour';
 
-	static function create($user) {
+	public static function create($user)
+	{
 		if (!static::checkCrudParams($user)) {
 			throw new \Katu\Exceptions\InputErrorException("Invalid arguments.");
 		}
 
-		return static::insert(array(
-			'timeCreated' => (string) (\Katu\Utils\DateTime::get()->getDbDateTimeFormat()),
-			'timeExpires' => (string) (\Katu\Utils\DateTime::get(static::EXPIRES)->getDbDateTimeFormat()),
-			'userId'      => (int)    ($user->getId()),
-			'token'       => (string) (\Katu\Utils\Random::getString(static::getColumn('token')->getProperties()->length)),
-		));
+		return static::insert([
+			'timeCreated' => (string)new \Katu\Utils\DateTime,
+			'timeExpires' => (string)new \Katu\Utils\DateTime(static::EXPIRES),
+			'userId'      => (int)   $user->getId(),
+			'token'       => (string)\Katu\Utils\Random::getString(static::getColumn('token')->getProperties()->length),
+		]);
 	}
 
-	static function checkCrudParams($user) {
+	public static function checkCrudParams($user)
+	{
 		if (!$user || !($user instanceof User)) {
 			throw (new \Katu\Exceptions\InputErrorException("Invalid user."))
 				->addErrorName('user')
@@ -31,15 +33,16 @@ class UserPasswordToken extends \Katu\Model {
 		return true;
 	}
 
-	public function isValid() {
+	public function isValid()
+	{
 		return \Katu\Utils\DateTime::get($this->timeExpires)->isInFuture() && !$this->timeUsed;
 	}
 
-	public function expire() {
+	public function expire()
+	{
 		$this->update('timeUsed', \Katu\Utils\DateTime::get()->getDbDateTimeFormat());
 		$this->save();
 
 		return true;
 	}
-
 }

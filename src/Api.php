8506a99e2f7a4094cd8c57814e954e95a23f71f8
Jan Jablunka <jablunka@gmail.com>
2020-05-21 12:41:52 +0200

    incremental

--- src/Api.php
+++ src/Api.php
@@ -2,16 +2,18 @@
 
 namespace Katu;
 
-class Api {
-
-	static function success($res = null, $options = []) {
+class Api
+{
+	public static function success($res = null, $options = [])
+	{
 		$app = App::get();
 		$app->response->setStatus(200);
 
 		return Utils\JSON::respond($res, $options);
 	}
 
-	static function error($error = null, $code = null) {
+	public static function error($error = null, $code = null)
+	{
 		$errors = [];
 
 		if ($error instanceof \Katu\Exceptions\ExceptionCollection) {
@@ -46,23 +48,26 @@ class Api {
 		return static::errors($res);
 	}
 
-	static function errors($res = []) {
+	public static function errors($res = [])
+	{
 		$app = App::get();
 		$app->response->setStatus(400);
 
 		return Utils\JSON::respond($res);
 	}
 
-	static function getUrl($endpoint) {
+	public static function getUrl($endpoint)
+	{
 		return Utils\Url::joinPaths(Config::get('app', 'apiUrl'), $endpoint);
 	}
 
-	static function get($endpoint, $params = [], &$curl = null) {
+	public static function get($endpoint, $params = [], &$curl = null)
+	{
 		return \Amiko\Amiko::get(static::getUrl($endpoint), $params, $curl);
 	}
 
-	static function post($endpoint, $params = [], &$curl = null) {
+	public static function post($endpoint, $params = [], &$curl = null)
+	{
 		return \Amiko\Amiko::post(static::getUrl($endpoint), $params, $curl);
 	}
-
 }

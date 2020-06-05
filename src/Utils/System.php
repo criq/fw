f699fc6c0e4d5fa125ddce3d055eee2b36eaf2db
Jan Jablunka <jablunka@gmail.com>
2020-06-05 18:40:30 +0200

    incremental

--- src/Utils/System.php
+++ src/Utils/System.php
@@ -2,64 +2,57 @@
 
 namespace Katu\Utils;
 
-class System {
-
-	static function getNumberOfCpus() {
-		return Cache::getFromMemory('system.numberOfCpus', function() {
-
+class System
+{
+	public static function getNumberOfCpus()
+	{
+		return Cache::getFromMemory('system.numberOfCpus', function () {
 			$numCpus = 1;
 			if (is_file('/proc/cpuinfo')) {
-
 				$cpuinfo = file_get_contents('/proc/cpuinfo');
 				preg_match_all('/^processor/m', $cpuinfo, $matches);
 				$numCpus = count($matches[0]);
-
 			} elseif ('WIN' == strtoupper(substr(PHP_OS, 0, 3))) {
-
 				$process = @popen('wmic cpu get NumberOfCores', 'rb');
 				if (false !== $process) {
 					fgets($process);
 					$numCpus = intval(fgets($process));
 					pclose($process);
 				}
-
 			} else {
-
 				$process = @popen('sysctl -a', 'rb');
 				if (false !== $process) {
-
 					$output = stream_get_contents($process);
 					preg_match('/hw.ncpu: (\d+)/', $output, $matches);
 					if ($matches) {
 						$numCpus = intval($matches[1][0]);
 					}
 					pclose($process);
-
 				}
-
 			}
 
 			return $numCpus;
-
 		});
 	}
 
-	static function getLoadAverage() {
+	public static function getLoadAverage()
+	{
 		return sys_getloadavg();
 	}
 
-	static function getLoadAveragePerCpu() {
-		return array_map(function($i) {
+	public static function getLoadAveragePerCpu()
+	{
+		return array_map(function ($i) {
 			return $i / static::getNumberOfCpus();
 		}, static::getLoadAverage());
 	}
 
-	static function assertMaxLoadAverage($loadAverage) {
+	public static function assertMaxLoadAverage($loadAverage)
+	{
 		if (static::getLoadAveragePerCpu()[0] > $loadAverage) {
 			throw new \Katu\Exceptions\LoadAverageExceededException("System load average per CPU is higher than " . $loadAverage . ".");
 		}
 
 		return true;
 	}
-
 }
